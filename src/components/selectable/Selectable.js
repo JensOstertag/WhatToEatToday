c8e7c5af110f6c9a23ebd21240689decfdddf7f3
JensOstertag <JensOstertag@gmx.de>
2022-10-07 16:03:09 +0200

    Selectable Component

---
+++ src/components/selectable/Selectable.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Selectable.css';
+
+class Selectable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "name": this.props.name,
+            "title": this.props.title,
+            "description": this.props.description,
+            "image": process.env.PUBLIC_URL + "/img/selectables/" + this.props.image,
+            "linkTo": this.props.linkTo,
+            "selectKey": this.props.selectKey,
+            "selectValue": this.props.selectValue
+        }
+    }
+
+    setStorage() {
+        sessionStorage.setItem(this.state.selectKey, this.state.selectValue);
+    }
+    
+    render() {
+        return (
+            <Link to={this.state.linkTo} onClick={() => {
+                this.setStorage();
+            }} className="selectable">
+                <img src={this.state.image} />
+                <p>{this.state.title}</p>
+                <p className='nomargin'><span>{this.state.description}</span></p>
+            </Link>
+        );
+    }
+}
+
+export default Selectable;
