da7fb59b7cce1fffd944a81c844e71fdd080885f
JensOstertag <JensOstertag@gmx.de>
2022-10-15 09:54:22 +0200

    Eating Habits Component

---
+++ src/pages/eating-habits/EatingHabits.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Selectable from '../../components/selectable/Selectable';
+import Selector from '../../components/selector/Selector';
+
+class EatingHabits extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
+
+  render() {
+    console.log(this.props.location);
+    return (
+      <section>
+        <p>Please Select one of the following Eating Habits.</p>
+        <Selector>
+          <Selectable name="vegetarian" title="Vegetarian" description={["Only show vegetarian food options"]} image="eating-habits-vegetarian.svg" linkTo="/result" selectKey="eating-habits" selectValue="vegetarian" />
+          <Selectable name="vegan" title="Vegan" description={["Only show vegan food options"]} image="eating-habits-vegan.svg" linkTo="/result" selectKey="eating-habits" selectValue="vegan" />
+          <Selectable name="none" title="None" description={["Show all options"]} image="eating-habits-none.svg" linkTo="/result" selectKey="eating-habits" selectValue="none" />
+        </Selector>
+      </section>
+    );
+  }
+}
+
+export default EatingHabits;
