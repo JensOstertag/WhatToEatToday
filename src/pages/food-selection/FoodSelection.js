a7f4704cd4d00fa22dca961b179855ed57c1bb40
JensOstertag <JensOstertag@gmx.de>
2022-11-03 14:26:32 +0100

    Add Garnish Image in Food Selection

--- src/pages/food-selection/FoodSelection.js
+++ src/pages/food-selection/FoodSelection.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
         <p>Please select a food type you want to generate.</p>
         <Selector>
           <Selectable name="" title="Complete Dinner" description={["Decide over a complete dinner with garnish and a main ingredient"]} image="" linkTo="/eating-habits" selectKey="food-type" selectValue="complete" />
-          <Selectable name="" title="Garnish" description={["Decide over what to cook as a side dish"]} image="" linkTo="/eating-habits" selectKey="food-type" selectValue="garnish" />
+          <Selectable name="" title="Garnish" description={["Decide over what to cook as a side dish"]} image="food-types-garnish.svg" linkTo="/eating-habits" selectKey="food-type" selectValue="garnish" />
           <Selectable name="" title="Main Ingredient" description={["Decide over what to cook as a main ingredient"]} image="" linkTo="/eating-habits" selectKey="food-type" selectValue="main" />
           <Selectable name="" title="Fast Food" description={["Decide over a Dinner made with frozen goods"]} image="food-types-frozen.svg" linkTo="/eating-habits" selectKey="food-type" selectValue="fast-food" />
           <Selectable name="" title="Restaurant" description={["Decide over what restaurant type to visit or from where to order"]} image="food-types-restaurant.svg" linkTo="/eating-habits" selectKey="food-type" selectValue="restaurant" />
